63ac0c640eb9cbb54f89be39065c3020a0a75533
SammioX <95929131+SammioX@users.noreply.github.com>
2021-12-11 21:06:02 +0530

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -32,70 +32,35 @@ async def _human_time_duration(seconds):
     
    
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
-async def start_(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://telegra.ph/file/f323bcaec71ba138fb6df.png",
-        caption=f"""**A Telegram Music Bot Based Mongodb.
- Add Me To Ur Chat For and Help and And Support Click On Buttons  ...
-💞  These Features A.I Based 
-Powered By [ᴢᴀɪᴅ ʙᴏᴛꜱ](t.me/superior_bots) ...
-**""",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "➕ ❰ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ❱ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅꜱ", url=f"https://t.me/SUPERIOR_BOTS/160"
-                    ),
-                    InlineKeyboardButton(
-                        "ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url="https://github.com/ITZ-ZAID/Zaid-Vc-Player"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "📢 ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"
-                    ),
-                    InlineKeyboardButton(
-                        "ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 🇮🇳", url=f"https://t.me/{ZAID_SUPPORT}"
-                    )
-                ]
-                
-           ]
-        ),
-    )
+
     
     
-@Client.on_message(commandpro(["/start", "/alive"]) & filters.group & ~filters.edited)
+@Client.on_message(commandpro(["/start"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/dd9ca2b2122dd68ffab0e.png",
-        caption=f"""Thanks For Adding Me To Ur Chat, For Any Query U Can Join Our Support Groups 🔥♥️""",
+        photo=f"https://telegra.ph/file/e3c1e773a8e0fd11f53ed.jpg",
+        caption=f"""𝐍𝐎𝐈𝐍𝐎𝐈 𝐌𝐔𝐒𝐈𝐂 🎶 𝗦𝗨𝗣𝗘𝗥𝗙𝗔𝗦𝗧 𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 ᴊᴏɪɴ ʜᴇʀᴇ 💞", url=f"https://t.me/{SUPPORT_GROUP}")
+                        "💥 ᴊᴏɪɴ ʜᴇʀᴇ 💞", url=f"https://t.me/CFC_BOT_SUPPORT")
                 ]
             ]
         ),
     )
 
 
-@Client.on_message(command(["repo", "source"]) & filters.group & ~filters.edited)
+@Client.on_message(command(["noinoi", "bazi"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/92688f2c44a35ba673c23.png",
-        caption=f"""Here Is The Source Code Fork And Give Stars ✨""",
+        photo=f"https://telegra.ph/file/e3c1e773a8e0fd11f53ed.jpg",
+        caption=f"""𝐍𝐎𝐈𝐍𝐎𝐈 𝐌𝐔𝐒𝐈𝐂 🎶 𝗦𝗨𝗣𝗘𝗥𝗙𝗔𝗦𝗧 𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧✨ \n\n ✨ FEATURS OF NOINOI \n ✨ MUSIC PLAYER \n ✨ GROUP MANAGER \n ✨ MENTION BOT \n ✨ COMING LOGO...""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        " ʀᴇᴘᴏ ⚒️", url=f"https://github.com/ITZ-ZAID/Zaid-Vc-Player")
+                        " BAZIGAR ‼", url=f"https://t.me/bazigarYt")
                 ]
             ]
         ),
